1500cb0ed67071698a921100ea36d67cbc3ea6c2
alre1394 <arenlund1@gmail.com>
2026-09-10 19:20:07 +0200

    Data and function documentation, license generation

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Palmer penguins dataset
+#'
+#' Measurements for penguins observed in the Palmer Archipelago, Antarctica.
+#'
+#' @format A data frame with 344 rows and 8 columns:
+#' \describe{
+#'   \item{species}{Penguin species: Adelie, Chinstrap, or Gentoo.}
+#'   \item{island}{Island where the penguin was observed.}
+#'   \item{bill_len}{Bill length in millimetres.}
+#'   \item{bill_dep}{Bill depth in millimetres.}
+#'   \item{flipper_len}{Flipper length in millimetres.}
+#'   \item{body_mass}{Body mass in grams.}
+#'   \item{sex}{Penguin sex.}
+#'   \item{year}{Year of observation.}
+#' }
+#' @source
+#' Horst AM, Hill AP, Gorman KB (2020).
+#' Palmer Archipelago penguin data.
+#' Available at \url{https://allisonhorst.github.io/palmerpenguins/}.
+#' @usage data(penguins)
+#' @keywords datasets
+#' @name penguins
+"penguins"
